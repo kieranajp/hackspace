6ecf50e662bc09a248dd1a0c6d2863b76c8af3b7
Kieran <kieranajp@me.com>
2012-03-03 22:53:40 +0000

    Events captured (using constant-polling method), converted to human-readable console.logs

--- controller.js
+++ controller.js
@@ -3,27 +3,186 @@ var controllerTimer;
 
 function onGamepadConnected(e) {
 	controller = e.gamepad;
-	console.log("Gamepad connected", controller.id);
-  controllerTimer = setInterval(checkState, 100);
+	createEvent("connected");
+  controllerTimer = setInterval(checkState, 80);
 }
 
 function onGamepadDisconnected(e) {
-	console.log("Gamepad disconnected", e.controller.id);
+	createEvent("disconnected");
 }
 
 function checkState() {
 	for (var i = 0; i < controller.buttons.length; i++) {
-		if (controller.buttons[i] != 0) {
-			console.log("Button state", i, controller.buttons[i]);
+		switch (i) {
+			case 0:
+				if (controller.buttons[i] == 1) {
+					createEvent("x");
+				}
+				break;
+			case 1:
+				if (controller.buttons[i] == 1) {
+					createEvent("a");
+				}
+				break;
+			case 2:
+				if (controller.buttons[i] == 1) {
+					createEvent("b");
+				}
+				break;
+			case 3:
+				if (controller.buttons[i] == 1) {
+					createEvent("y");
+				}
+				break;
+			case 4:
+				if (controller.buttons[i] == 1) {
+					createEvent("lb");
+				}
+				break;
+			case 5:
+				if (controller.buttons[i] == 1) {
+					createEvent("rb");
+				}
+				break;
+			case 6:
+				if (controller.buttons[i] == 1) {
+					createEvent("lt");
+				}
+				break;
+			case 7:
+				if (controller.buttons[i] == 1) {
+					createEvent("rt");
+				}
+				break;
+			case 8:
+				if (controller.buttons[i] == 1) {
+					createEvent("back");
+				}
+				break;
+			case 9:
+				if (controller.buttons[i] == 1) {
+					createEvent("start");
+				}
+				break;
+			case 10:
+				if (controller.buttons[i] == 1) {
+					createEvent("left-click");
+				}
+				break;
+			case 11:
+				if (controller.buttons[i] == 1) {
+					createEvent("right-click");
+				}
+				break;
 		}
 	}
 
 	for (var j = 0; j < controller.axes.length; j++) {
-		if (controller.axes[j] < -0.05 || controller.axes[j] > 0.05) {
-			console.log("Axis state", j, controller.axes[j]);
+		switch (j) {
+			case 1:
+				if (controller.axes[j] < -0.05) {
+					createEvent("left-left");
+				} else if (controller.axes[j] > 0.05) {
+					createEvent("left-right");
+				}
+				break;
+			case 2:
+				if (controller.axes[j] < -0.05) {
+					createEvent("left-up");
+				} else if (controller.axes[j] > 0.05) {
+					createEvent("left-down");
+				}
+				break;
+			case 3:
+				if (controller.axes[j] < -0.05) {
+					createEvent("right-left");
+				} else if (controller.axes[j] > 0.05) {
+					createEvent("right-right");
+				}
+				break;
+			case 4:
+				if (controller.axes[j] < -0.05) {
+					createEvent("right-up");
+				} else if (controller.axes[j] > 0.05) {
+					createEvent("right-down");
+				}
+				break;
 		}
 	}
 }
 
 window.addEventListener("MozGamepadConnected", onGamepadConnected);
 window.addEventListener("MozGamepadDisconnected", onGamepadDisconnected);
+
+
+
+function createEvent(type) {
+	switch (type) {
+		case "connected":
+			console.log("Gamepad connected" );
+			break;
+		case "disconnected":
+			console.log("Gamepad disconnected" );
+			break;
+		case "left-up":
+			console.log("Left stick north" );
+			break;
+		case "left-down":
+			console.log("Left stick south" );
+			break;
+		case "left-right":
+			console.log("Left stick east" );
+			break;
+		case "left-left":
+			console.log("Left stick west" );
+			break;
+		case "right-up":
+			console.log("Right stick north" );
+			break;
+		case "right-down":
+			console.log("Right stick south" );
+			break;
+		case "right-right":
+			console.log("Right stick east" );
+			break;
+		case "right-left":
+			console.log("Right stick west" );
+			break;
+		case "a":
+			console.log("A pressed");
+			break;
+		case "b":
+			console.log("B pressed");
+			break;
+		case "x":
+			console.log("X pressed");
+			break;
+		case "y":
+			console.log("Y pressed");
+			break;
+		case "lb":
+			console.log("LB pressed");
+			break;
+		case "rb":
+			console.log("RB pressed");
+			break;
+		case "lt":
+			console.log("LT pressed");
+			break;
+		case "rt":
+			console.log("RT pressed");
+			break;
+		case "back":
+			console.log("Back pressed");
+			break;
+		case "start":
+			console.log("Start pressed");
+			break;
+		case "left-click":
+			console.log("Left stick clicked down");
+			break;
+		case "right-click":
+			console.log("Right stick clicked down");
+			break;
+	}
+}
