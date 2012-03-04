6f4a76508e22092ddb920b9b9b07604779b010f4
Tom Gallacher <tomgallacher23@gmail.com>
2012-03-04 03:55:46 +0000

    Adding Players and Game to the whole shabang

--- server/app.js
+++ server/app.js
@@ -3,6 +3,11 @@ var express = require('express')
   , app = require('express').createServer()
   , io = require('socket.io').listen(app)
   , fs = require('fs')
+  , Game = require('../public/ServerGame')
+  , Player = require('../public/Player')
+  , CurrentMovesRegistry = require('./currentMovesRegistry').CurrentMovesRegistry
+  , game = new Game()
+  , currentMovesRegistry = new CurrentMovesRegistry()
   ;
 
 app.configure(function(){
@@ -14,23 +19,24 @@ app.configure(function(){
   app.use(app.router);
 });
 
-io.sockets.on('controllerConnected', function (socket) {
+io.sockets.on('connection', function (socket) {
   socket.emit('connected', { success: true, player: true });
-  socket.on('control', function (data) {
-		// This will have the controls coming into the node instance, we will need
-		// to sort these by id's and ensure that they are not mutable by each other.
-  });
+  // socket.on('control', function (data) {
+		// // This will have the controls coming into the node instance, we will need
+		// // to sort these by id's and ensure that they are not mutable by each other.
+  // });
 
   socket.on('controllerConnected', function() {
-
+    Game.addPlayer(socket.id, new Player());
   });
 
   socket.on('controllerDisconnected', function() {
-
+    Game.removePlayer(socket.id);
   });
 
   socket.on('move', function(data /* { direction {n,e,s,w} } */) {
-    
+    // Game.movePlayer(socket.id);
+    currentMovesRegistry.store(socket.id, data);
   });
 });
 
@@ -49,10 +55,11 @@ app.get('/', function (req, res) {
   });
 });
 
-var game = new Game();
-
 process.nextTick(function() {
   // This will be the game loop.
+  currentMovesRegistry.retrieveAll(function(data) {
+    Object.keys(data).forEach(Game.processMove());
+  });
 });
 
 
